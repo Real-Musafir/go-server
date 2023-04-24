2500ab1615c3b5a1c0eeec5d8e0e99fbccb82c3b
Real-Musafir <alshahadath@gmail.com>
2023-04-24 15:49:29 +0600

    fixed form submit route issue

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request){
-	if err := r.ParseForm; err!=nil {
+	if err := r.ParseForm(); err!=nil {
 		fmt.Fprintf(w, "ParseForm() err %v", err)
 		return
 	}
